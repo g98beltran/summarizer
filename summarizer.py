268e02233610684e565150a926a10479d1284d64
Gerardo <6erar8eltran@gmail.com>
2022-07-18 13:02:21 +0200

    arguments and pandas
    now we use pandas to read the csv file and accept arguments in the call of the program

--- summarizer.py
+++ summarizer.py
@@ -1,15 +1,34 @@
-import csv
+import sys
+import pandas as pd
 
-with open('data.csv') as File:
-    reader = csv.reader(File, delimiter=',', quotechar=',',
-                        quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        for x in row:
-            print(x)
-            print(type(x))
-            try:
-                print(int(x))
-            except:
-                print("No es int")
-            if isinstance(x,int):
-                print(x)
+arguments = len(sys.argv)-1
+print(arguments)
+
+if(arguments == 1):
+    csv= sys.argv[1]
+    data=pd.read_csv(csv)
+
+    print(data.dtypes)
+else:
+    print("python summarizer.py data.csv")
+
+    
+#import csv
+
+#with open('data.csv') as File:
+#    reader = csv.reader(File, delimiter=',', quotechar=',',
+#                        quoting=csv.QUOTE_MINIMAL)
+    
+
+
+    
+#    for row in reader:
+#         for x in row:
+#            print(x)
+#             print(type(x))
+#             try:
+#                 print(int(x))
+#             except:
+#                 print("No es int")
+#             if isinstance(x,int):
+#                 print(x)
